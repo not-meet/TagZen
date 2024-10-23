de4d613e5437e0f5c287179f4e0021af716f4f2b
not-meet <mjmeet.279@gmail.com>
2024-10-23 23:03:53 +0530

    added the base of project

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  const navIcons = [
+    { src: "/assets/icons/search.svg", alt: 'search' },
+    { src: "/assets/icons/black-heart.svg", alt: 'heart' },
+    { src: "/assets/icons/user.svg", alt: 'user' }
+
+  ]
+  return (
+    <header className="w-full">
+      <nav className="nav">
+        <Link href="/" className="flex items-center gap-1">
+          <Image src="/assets/icons/logo.svg" width={27} height={27} alt="logo" />
+          <p className="nav-logo">Tag<span className="text-blue-900">Zen</span></p>
+        </Link>
+
+        <div className="flex items-center gap-5">
+          {navIcons.map((icons) => (
+            <Image key={icons.alt} src={icons.src} alt="logo" className="object-contain" width={28} height={28} />
+          ))}
+        </div>
+      </nav>
+    </header>
+  )
+}
+export default Navbar
