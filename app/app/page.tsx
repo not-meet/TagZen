56f5f8efa439449fb2ed7e422339aacadc9922e3
not-meet <mjmeet.279@gmail.com>
2024-10-25 00:15:30 +0530

    added the home page will start working on the web srcaping setup

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Image from "next/image";
+import SearchBar from "@/components/SearchBar";
+import HeroCarousel from "@/components/HeroCarousel";
 export default function Home() {
   return (
     <>
-      <section className="px-6 md:px-20 py-24 border-2 border-red-500">
+      <section className="px-6 md:px-20 py-16">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
             <p className="small-text">Smart shopping starts here:
@@ -12,9 +14,10 @@ export default function Home() {
 
             <h1 className="head-text">Get Steps Ahead with <span className="text-primary">TagZen!</span></h1>
             <p className="mt-6">Powerfull, self-serve product and growth analytics to help you convert, engage and retain more..</p>
-            SearchBar
+            <SearchBar />
           </div>
-          Hero Carousel
+
+          <HeroCarousel />
         </div>
       </section>
       <section className="trending-section">
---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,50 @@
+"use client"
+import React, { FormEvent, useState } from "react";
+
+const isValidAmazonProductURL = (url: string) => {
+  try {
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+    if (
+      hostname.includes('amazon.com') || hostname.includes('amazon.') || hostname.endsWith('amazon')
+    ) {
+      return true;
+    }
+  } catch (e) {
+    return false;
+  }
+  return false;
+}
+
+const SearchBar = () => {
+  const [searchpromt, setSearchPromt] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const isValidLink = isValidAmazonProductURL(searchpromt);
+
+    if (!isValidLink) return alert('please provide a valid amazon link');
+
+    try {
+      setIsLoading(true)
+    } catch (e) {
+      console.log(e)
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-wrap gap-4 mt-12">
+      <input type="text"
+        placeholder="enter product link"
+        value={searchpromt}
+        onChange={(e) => setSearchPromt(e.target.value)}
+        className="searchbar-input" />
+      <button type="submit" className="searchbar-btn" disabled={searchpromt === ''}>{isLoading ? 'Searching....' : "Search"}</button>
+    </form>
+  )
+}
+
+export default SearchBar;
