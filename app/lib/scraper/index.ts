f5c3c0373ca4994adf652820cf56bfb01f8065e3
not-meet <mjmeet.279@gmail.com>
2024-11-02 18:08:02 +0530

    seted up the scraping thingy

--- app/lib/scraper/index.ts
+++ app/lib/scraper/index.ts
@@ -1,5 +1,29 @@
+import axios from "axios";
+
 export async function scrapeAmazonProduct(url: string) {
   if (!url) return;
   //bright data proxy config
+  const username = String(process.env.BRIGHT_DATA_USERNAME);
+  const password = String(process.env.BRIGHT_DATA_PASSWORD);
+
+  const port = 22225;
+  const session_id = (1000000 * Math.random()) | 0;
+  const options = {
+    auth: {
+      username: `${username}-session-${session_id}`,
+      password,
+    },
+    host: 'brd.superproxy.io',
+    port,
+    rejectUnauthorized: false,
+  }
+
+  try {
+    //fetching the product
+    const response = await axios.get(url, options);
+    console.log(response.data);
+  } catch (error: any) {
+    throw new Error(`Failed to scrape product: ${error.message}`);
+  }
 
 }
