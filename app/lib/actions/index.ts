de97530b8583572423e3943de1b143c0c80c0405
not-meet <mjmeet.279@gmail.com>
2024-11-04 02:59:26 +0530

    added mogodb connect and schema!

--- app/lib/actions/index.ts
+++ app/lib/actions/index.ts
@@ -1,7 +1,10 @@
 "use server"
 
+import { revalidatePath } from 'next/cache'
+import Product from "../models/product.models";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from '../utils'
 
 export async function scrapAndStoreProducts(productUrl: string) {
   if (!productUrl) return;
@@ -11,6 +14,21 @@ export async function scrapAndStoreProducts(productUrl: string) {
 
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
     if (!scrapedProduct) return
+
+    let product = scrapedProduct;
+    const existingProduct = await Product.findOne({ url: scrapedProduct.url });
+    if (existingProduct) {
+      const updatedPriceHistory: any = [...existingProduct.priceHistory, { price: scrapedProduct.currentPrice }]
+
+      product = {
+        ...scrapedProduct, priceHistory: updatedPriceHistory, lowestPrice: getLowestPrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory), averagePrice: getAveragePrice(updatedPriceHistory),
+
+      }
+    }
+    const newProduct = await Product.findOneAndUpdate({ url: scrapedProduct.url }, product, { upsert: true, new: true })
+
+    revalidatePath(`/products/${newProduct._id}`);
   } catch (error: any) {
     throw new Error(`failed to create/update product: ${error.message}`);
   }
